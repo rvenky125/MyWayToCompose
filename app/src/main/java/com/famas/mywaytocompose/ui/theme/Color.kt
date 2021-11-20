87d33f462458e2f84e7add95fd9fcaddf202fd45
rvenky125 <venkypaithireddy@gmail.com>
2021-11-20 19:45:45 +0530

    Initial commit

---
+++ app/src/main/java/com/famas/mywaytocompose/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.famas.mywaytocompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val progressColor1 = Color(0xFF7dd88e)
+val progressColor2 = Color(0xFFfb9a50)
+val progressColor3 = Color(0xFFcc4344)
+val progressColor4 = Color(0xFF90427a)
+val progressColor5 = Color(0xFF1dd1fa)
